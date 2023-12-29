c8bfc8eca0d7a61dc4c3512aabf7a5d7a0f2c34f
Zachary Lee <leeyihong03@gmail.com>
2023-12-29 14:48:15 +0800

    fixed db transactions and changed env name

--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,5 @@
-import mysql, { Connection, PoolConnection, RowDataPacket, ResultSetHeader, ProcedureCallPacket } from 'mysql2/promise';
+import mysql, { Connection, RowDataPacket, ResultSetHeader, ProcedureCallPacket } from 'mysql2/promise';
+import { TransactionQueries, Query } from './interface';
 
 class Database {
     private static instance: Database;
@@ -16,13 +17,23 @@ class Database {
     public async getConnection(): Promise<Connection> {
         if (this.connection === null) {
             try {
-                this.connection = await mysql.createConnection({
-                    host: process.env.DATABASE_HOST,
-                    user: process.env.DATABASE_USER,
-                    password: process.env.DATABASE_PASSWORD,
-                    database: process.env.DATABASE_NAME,
-                    port: parseInt(process.env.DATABASE_PORT || '3306')
-                });
+                if (process.env.IS_PRODUCTION === 'false') {
+                    this.connection = await mysql.createConnection({
+                        host: process.env.DATABASE_HOST_DEV,
+                        user: process.env.DATABASE_USER_DEV,
+                        password: process.env.DATABASE_PASSWORD_DEV,
+                        database: process.env.DATABASE_NAME_DEV,
+                        port: parseInt(process.env.DATABASE_PORT_DEV || '3306')
+                    });
+                } else {
+                    this.connection = await mysql.createConnection({
+                        host: process.env.DATABASE_HOST,
+                        user: process.env.DATABASE_USER,
+                        password: process.env.DATABASE_PASSWORD,
+                        database: process.env.DATABASE_NAME,
+                        port: parseInt(process.env.DATABASE_PORT || '3306')
+                    });
+                }
             } catch (error) {
                 console.error('Error connecting to database:', error);
                 throw error;
@@ -31,21 +42,26 @@ class Database {
         return this.connection;
     }
 
-    public async query<T>(sql: string, params?: any[]): Promise<T> {
+    public async query<T>(query: Query): Promise<T> {
         const connection = await this.getConnection();
-        const [results] = await connection.execute<ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | ResultSetHeader[] | ProcedureCallPacket>(sql, params);
+        const [results] = await connection.execute<ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | ResultSetHeader[] | ProcedureCallPacket>(query.sql, query.params);
         return results as T;
     }
 
-    public async transactionQuery<T>(
-        client: PoolConnection, sql: string, params?: any[]
-    ): Promise<T> {
+    public async executeTransaction(queries: TransactionQueries): Promise<void> {
+        const connection = await this.getConnection();
         try {
-            const [results] = await client.execute<ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | ResultSetHeader[] | ProcedureCallPacket>(sql, params);
-            return results as T;
+            await connection.beginTransaction();
+            for (const query of queries) {
+                await connection.execute<ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | ResultSetHeader[] | ProcedureCallPacket>(query.sql, query.params);
+            }
+            await connection.commit();
         } catch (error) {
-            console.error('Error executing transaction query:', error);
+            await connection.rollback();
+            console.error('Transaction failed:', error);
             throw error;
+        } finally {
+            connection.end();
         }
     }
 }
