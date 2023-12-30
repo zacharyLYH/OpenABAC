007af461385b93943ed955380d76c6fcaae2f1fe
Zac <yihongle@buffalo.edu>
2023-12-30 13:28:17 +0800

    use the Query interface

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,16 @@
-import { db } from "@/lib/database"
-import { NextResponse } from "next/server";
+import { db } from '@/lib/database';
+import { Query } from '@/lib/interface';
+import { NextResponse } from 'next/server';
 
 interface QueryResult {
     TEST: number; // Adjust the type based on your actual query result
 }
 
 export async function GET() {
-    const result = await db.query<QueryResult[]>("SELECT 1 + 1 AS TEST");
+    const query: Query = {
+        sql: `SELECT 1 AS TEST`,
+        params: [],
+    };
+    const result = await db.query<QueryResult[]>(query);
     return NextResponse.json({ message: result[0].TEST }, { status: 200 });
 }
