4d37eed94d960e6512cdf15f1763eeae064ab6c6
Zac <yihongle@buffalo.edu>
2024-01-02 07:38:01 +0800

    linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default async function Home() {
-  const resp = await fetch("http://localhost:3000/api/test")
-  const result = await resp.json()
+    const resp = await fetch('http://localhost:3000/api/test');
+    const result = await resp.json();
 
-  return (
-    <div>
-      hi
-      {result.message}
-    </div>
-  )
+    return (
+        <div>
+            hi
+            {result.message}
+        </div>
+    );
 }
