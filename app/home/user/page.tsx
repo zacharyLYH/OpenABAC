dc49dba0bc37246a4b745a303b9ed2658d96c705
Zachary Lee <leeyihong03@gmail.com>
2024-01-02 13:11:31 +0800

    simplify landing page for development

---
+++ app/home/user/page.tsx
@@ -0,0 +1,9 @@
+export default async function UserPage() {
+    return (
+        <div>
+            1. Show users paginated
+            2. A dropdown filter
+            3. An option
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 export default async function Home() {
-    const resp = await fetch('http://localhost:3000/api/test');
-    const result = await resp.json();
-
     return (
-        <div>
-            hi
-            {result.message}
-        </div>
+        <main>
+            Landing page
+        </main>
     );
 }
