7d2c98b6baa061ea804e9ef71df555a1acd8f537
Zac <yihongle@buffalo.edu>
2024-01-03 11:17:14 +0800

    add a naive db health check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default async function Home() {
+    const resp = await fetch('http://localhost:3000/api/test', {
+        cache: 'no-store',
+    });
+    const result = await resp.json();
     return (
         <main>
             Landing page
+            {result.message}
         </main>
     );
 }
