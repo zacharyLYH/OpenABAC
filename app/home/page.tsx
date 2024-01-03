b2d4c006aa641c8dd31123565af9b21fb4a9c28a
Zac <yihongle@buffalo.edu>
2024-01-03 11:17:57 +0800

    linting

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,67 +4,103 @@ import {
     CardDescription,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-} from "@/components/ui/tabs"
-import { Check, Container, FileWarningIcon, Move, Scroll, User } from "lucide-react"
-import ViewUserTab from "./_tab-options/user-tab"
+    Check,
+    Container,
+    FileWarningIcon,
+    Move,
+    Scroll,
+    User,
+} from 'lucide-react';
+import ViewUserTab from './_tab-options/user-tab';
+import { Suspense } from 'react';
+import { TableSuspenseSkeleton } from '@/components/table-suspense';
 
-function TabSectionCard({ title, value, description, icon }: { title: string, value: string, description?: string, icon?: JSX.Element }) {
+function TabSectionCard({
+    title,
+    value,
+    description,
+    icon,
+}: {
+    title: string;
+    value: string;
+    description?: string;
+    icon?: JSX.Element;
+}) {
     return (
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                    {title}
-                </CardTitle>
+                <CardTitle className="text-sm font-medium">{title}</CardTitle>
                 {icon}
             </CardHeader>
             <CardContent>
                 <div className="text-2xl font-bold">{value}</div>
-                <CardDescription>
-                    {description}
-                </CardDescription>
+                <CardDescription>{description}</CardDescription>
             </CardContent>
         </Card>
-    )
+    );
 }
 
 export default async function HomePage() {
-    return <div className="flex-1 space-y-4 p-8 pt-6">
-        <h2 className="text-5xl font-bold tracking-tight">Dashboard</h2>
-        <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
-                <TabsTrigger value="overview">Overview</TabsTrigger>
-                <TabsTrigger value="actions">
-                    Actions
-                </TabsTrigger>
-                <TabsTrigger value="user">
-                    Users
-                </TabsTrigger>
-                <TabsTrigger value="policies">
-                    Policies
-                </TabsTrigger>
-                <TabsTrigger value="context">
-                    Contexts
-                </TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview" className="space-y-4">
-                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                    <TabSectionCard title="#Users" value="$45,231.89" description="+20.1% from last month" icon={<User className="h-5 w-5" />} />
-                    <TabSectionCard title="#Policies" value="+2350" description="+180.1% from last month" icon={<Scroll className="h-5 w-5" />} />
-                    <TabSectionCard title="#Actions" value="+12,234" description="+19% from last month" icon={<Move className="h-5 w-5" />} />
-                    <TabSectionCard title="#Context" value="+573" description="+201 since last hour" icon={<Container className="h-5 w-5" />} />
-                    <TabSectionCard title="#Successful Authorizations" value="+573" description="+201 since last hour" icon={<Check className="h-5 w-5" />} />
-                    <TabSectionCard title="#Failed Authorizations" value="+573" description="+201 since last hour" icon={<FileWarningIcon className="h-5 w-5" />} />
-                </div>
-            </TabsContent>
-            <TabsContent value="user">
-                <ViewUserTab />
-            </TabsContent>
-        </Tabs>
-    </div>
+    return (
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <h2 className="text-5xl font-bold tracking-tight">Dashboard</h2>
+            <Tabs defaultValue="overview" className="space-y-4">
+                <TabsList>
+                    <TabsTrigger value="overview">Overview</TabsTrigger>
+                    <TabsTrigger value="actions">Actions</TabsTrigger>
+                    <TabsTrigger value="user">Users</TabsTrigger>
+                    <TabsTrigger value="policies">Policies</TabsTrigger>
+                    <TabsTrigger value="context">Contexts</TabsTrigger>
+                </TabsList>
+                <TabsContent value="overview" className="space-y-4">
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                        <TabSectionCard
+                            title="#Users"
+                            value="$45,231.89"
+                            description="+20.1% from last month"
+                            icon={<User className="h-5 w-5" />}
+                        />
+                        <TabSectionCard
+                            title="#Policies"
+                            value="+2350"
+                            description="+180.1% from last month"
+                            icon={<Scroll className="h-5 w-5" />}
+                        />
+                        <TabSectionCard
+                            title="#Actions"
+                            value="+12,234"
+                            description="+19% from last month"
+                            icon={<Move className="h-5 w-5" />}
+                        />
+                        <TabSectionCard
+                            title="#Context"
+                            value="+573"
+                            description="+201 since last hour"
+                            icon={<Container className="h-5 w-5" />}
+                        />
+                        <TabSectionCard
+                            title="#Successful Authorizations"
+                            value="+573"
+                            description="+201 since last hour"
+                            icon={<Check className="h-5 w-5" />}
+                        />
+                        <TabSectionCard
+                            title="#Failed Authorizations"
+                            value="+573"
+                            description="+201 since last hour"
+                            icon={<FileWarningIcon className="h-5 w-5" />}
+                        />
+                    </div>
+                </TabsContent>
+                <TabsContent value="user">
+                    <Suspense fallback={<TableSuspenseSkeleton />}>
+                        <ViewUserTab />
+                    </Suspense>
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 }
