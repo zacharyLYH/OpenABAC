84f2b2c5696856f71e0febb2822e8a5b5dca0cd5
Zac <yihongle@buffalo.edu>
2024-01-03 13:09:40 +0800

    remove created date column

--- components/table/column-defs/user-column/user-column.tsx
+++ components/table/column-defs/user-column/user-column.tsx
@@ -1,50 +1,47 @@
-"use client"
+'use client';
 
-import { User } from "@/lib/interface"
-import { ColumnDef } from "@tanstack/react-table"
-import { DataTableColumnHeader } from "../../column-header";
-import { copyColumn, getPolicies, selectColumn } from "../column-template";
+import { User } from '@/lib/interface';
+import { ColumnDef } from '@tanstack/react-table';
+import { DataTableColumnHeader } from '../../column-header';
+import { copyColumn, getPolicies, selectColumn } from '../column-template';
 
 const renderJsonCell = (cell: any) => {
     const jsonStr = JSON.stringify(cell.getValue(), null, 2);
     const lines = jsonStr.split('\n');
     const maxLines = 5;
 
-    const displayedText = lines.length > maxLines
-        ? lines.slice(0, maxLines - 1).join('\n') + '\n...'
-        : jsonStr;
+    const displayedText =
+        lines.length > maxLines
+            ? lines.slice(0, maxLines - 1).join('\n') + '\n...'
+            : jsonStr;
 
     return <pre>{displayedText}</pre>;
 };
 
-
 export const userColumn: ColumnDef<User>[] = [
     ...selectColumn<User>(),
     {
-        accessorKey: "applicationUserId",
+        accessorKey: 'applicationUserId',
         header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Application User Id" />
-        )
+            <DataTableColumnHeader
+                column={column}
+                title="Application User Id"
+            />
+        ),
     },
     {
-        accessorKey: "jsonCol",
+        accessorKey: 'jsonCol',
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="JSON Column" />
         ),
-        cell: renderJsonCell
+        cell: renderJsonCell,
     },
     {
-        accessorKey: "modifiedDate",
+        accessorKey: 'modifiedDate',
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="Modified Date" />
-        )
-    },
-    {
-        accessorKey: "createdDate",
-        header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Created Date" />
-        )
+        ),
     },
     ...getPolicies<User>(),
-    ...copyColumn<User>()
+    ...copyColumn<User>(),
 ];
