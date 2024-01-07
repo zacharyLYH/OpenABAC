394d6b510aceffeea8148631a0ce3aec2b4dff42
Zac <yihongle@buffalo.edu>
2024-01-07 11:18:23 +0800

    change style to green and black

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from "@/components/providers/theme-provider"
-import { Toaster } from "@/components/ui/sonner"
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'OpenABAC',
-  description: 'Open Source Attribute Based Access Control (ABAC)',
-}
+    title: 'OpenABAC',
+    description: 'Open Source Attribute Based Access Control (ABAC)',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider></body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={cn(inter.className, 'bg-black')}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    disableTransitionOnChange
+                >
+                    {children}
+                    <Toaster />
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
