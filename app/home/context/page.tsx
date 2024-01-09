1a34d49ae1cef7e3ac5c9b2230623b160cf23495
Zachary Lee <leeyihong03@gmail.com>
2024-01-09 15:53:40 +0800

    implement create context page

--- app/home/context/page.tsx
+++ app/home/context/page.tsx
@@ -1,11 +1,25 @@
-import { CreateContextForm } from '@/components/edit-page-components/create-button/context/form';
-import { CreateButton } from '@/components/edit-page-components/create-button/create-button';
+"use client"
 
-export default async function ContextPage() {
+import { CreateContextForm } from '@/app/home/context/createContextForm';
+import { CreateButton } from '@/components/edit-page-components/create-button';
+import useContextStore from '@/zustand/edit-pages/context-store';
+import { AttachToAction } from './attachToAction';
+import { Separator } from '@/components/ui/separator';
+
+export default function ContextPage() {
+    const { createdContext } = useContextStore()
     return (
-        <div>
-            Edit Context data page
-            <CreateButton objName="Context" form={<CreateContextForm />} />
+        <div className='p-8'>
+            <div className='flex flex-row justify-between'>
+                <h2 className="text-5xl font-bold tracking-tight">Context</h2>
+                {createdContext ? null :
+                    <CreateButton objName="Context" form={<CreateContextForm />} />}
+            </div>
+            <Separator className='my-4' />
+            <div className='flex justify-center '>
+                {createdContext ? <AttachToAction /> : null}
+            </div>
+
         </div>
     );
 }
