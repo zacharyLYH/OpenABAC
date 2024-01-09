1de6bf4ffe981b78f5ed461dfa230b32106106a0
Zachary Lee <leeyihong03@gmail.com>
2024-01-09 15:53:22 +0800

    implemented text-bubble

---
+++ components/ui/text-bubble.tsx
@@ -0,0 +1,13 @@
+interface TextBubbleProps {
+    text: string
+}
+
+export const TextBubble: React.FC<TextBubbleProps> = ({ text }) => {
+    return (
+        <div className="flex-1 min-w-0 mx-4">
+            <div className="border border-white px-4 relative rounded">
+                <span className="block text-sm sm:inline">{text}</span>
+            </div>
+        </div>
+    );
+};
