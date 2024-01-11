74bcc7a846bb209f6ca7cc913bd646c5cca6c8b5
Zachary Lee <leeyihong03@gmail.com>
2024-01-11 16:46:13 +0800

    implement untested loading page

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <img src="/ball.svg" alt="Loading Animation" />
+        </div>
+    );
+};
+
