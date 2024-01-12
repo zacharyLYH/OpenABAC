b3da0c9dcd5e920306517ebd1f5a9eb223134c7e
Zachary Lee <leeyihong03@gmail.com>
2024-01-12 10:06:18 +0800

    add editClickedIndicator

--- zustand/edit-pages/context-store.ts
+++ zustand/edit-pages/context-store.ts
@@ -9,21 +9,23 @@ interface ContextStore {
     setActionsForSearch: (contexts: SearchAndSelectInterface[] | []) => void;
     selectedActionsFromSearch: SearchAndSelectInterface[] | [];
     setSelectedActionsFromSearch: (actions: SearchAndSelectInterface[] | []) => void;
+    editClickedIndicator: boolean;
+    setEditClickedIndicator: (clicked: boolean) => void;
 }
 
 const useContextStore = create<ContextStore>(set => ({
-    // createdContext: null,
-    createdContext: {
-        id: 'uuid-1',
-        contextDescription: 'Context Desc 1',
-        operator: 'equals',
-        entity: 'Entity1',
-        textValue: 'Text1',
-        timeValue1: '2023-01-01T00:00:00.000Z',
-        timeValue2: '2023-01-02T00:00:00.000Z',
-        modifiedDate: new Date('2023-01-01'),
-        createdDate: new Date('2023-01-01'),
-    },
+    createdContext: null,
+    // createdContext: {
+    //     id: 'uuid-1',
+    //     contextDescription: 'Context Desc 1',
+    //     operator: 'equals',
+    //     entity: 'Entity1',
+    //     textValue: 'Text1',
+    //     timeValue1: '2023-01-01T00:00:00.000Z',
+    //     timeValue2: '2023-01-02T00:00:00.000Z',
+    //     modifiedDate: new Date('2023-01-01'),
+    //     createdDate: new Date('2023-01-01'),
+    // },
     setCreatedContext: (context: Context | null) =>
         set({ createdContext: context }),
     actionsForSearch: [],
@@ -32,6 +34,9 @@ const useContextStore = create<ContextStore>(set => ({
     selectedActionsFromSearch: [],
     setSelectedActionsFromSearch: (actions: SearchAndSelectInterface[] | []) =>
         set({ selectedActionsFromSearch: actions }),
+    editClickedIndicator: false,
+    setEditClickedIndicator: (clicked: boolean) =>
+        set({ editClickedIndicator: clicked })
 }));
 
 export default useContextStore;
