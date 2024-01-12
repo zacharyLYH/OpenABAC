4e0c359466f9e4855f150e3dafee2c2edc84ae3b
Zachary Lee <leeyihong03@gmail.com>
2024-01-12 17:39:30 +0800

    implement mock update context by id

---
+++ lib/service/context/update-context.ts
@@ -0,0 +1,7 @@
+import { Context } from "@/lib/interface";
+
+export const updateContextById = async (data: Context) => {
+    if (process.env.IS_PRODUCTION === 'false') {
+        return data
+    }
+};
