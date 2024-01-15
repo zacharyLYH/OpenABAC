c537ed419e51d5ddb9ffaa05dee15eb5b344228c
Zachary Lee <leeyihong03@gmail.com>
2024-01-15 12:32:28 +0800

    implement state for delete indication

--- zustand/edit-pages/context-store.ts
+++ zustand/edit-pages/context-store.ts
@@ -11,21 +11,23 @@ interface ContextStore {
     setSelectedActionsFromSearch: (actions: SearchAndSelectInterface[] | []) => void;
     editClickedIndicator: boolean;
     setEditClickedIndicator: (clicked: boolean) => void;
+    deleteClickedIndicator: boolean;
+    setDeleteClickedIndicator: (clicked: boolean) => void;
 }
 
 const useContextStore = create<ContextStore>(set => ({
-    createdContext: null,
-    // createdContext: {
-    //     id: 'uuid-1',
-    //     contextDescription: 'Context Desc 1',
-    //     operator: 'equals',
-    //     entity: 'Entity1',
-    //     textValue: 'Text1',
-    //     timeValue1: '2023-01-01T00:00:00.000Z',
-    //     timeValue2: '2023-01-02T00:00:00.000Z',
-    //     modifiedDate: new Date('2023-01-01'),
-    //     createdDate: new Date('2023-01-01'),
-    // },
+    // createdContext: null,
+    createdContext: {
+        id: 'uuid-1',
+        contextDescription: 'Context Desc 1',
+        operator: 'equals',
+        entity: 'Entity1',
+        textValue: 'Text1',
+        timeValue1: '2023-01-01T00:00:00.000Z',
+        timeValue2: '2023-01-02T00:00:00.000Z',
+        modifiedDate: new Date('2023-01-01'),
+        createdDate: new Date('2023-01-01'),
+    },
     setCreatedContext: (context: Context | null) =>
         set({ createdContext: context }),
     actionsForSearch: [],
@@ -36,7 +38,10 @@ const useContextStore = create<ContextStore>(set => ({
         set({ selectedActionsFromSearch: actions }),
     editClickedIndicator: false,
     setEditClickedIndicator: (clicked: boolean) =>
-        set({ editClickedIndicator: clicked })
+        set({ editClickedIndicator: clicked }),
+    deleteClickedIndicator: false,
+    setDeleteClickedIndicator: (clicked: boolean) =>
+        set({ deleteClickedIndicator: clicked })
 }));
 
 export default useContextStore;
