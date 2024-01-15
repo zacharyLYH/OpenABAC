af7135db8ba341ce67e0ca6df26ba84d9fde6ddd
Zachary Lee <leeyihong03@gmail.com>
2024-01-15 12:29:33 +0800

    implement delete button

--- app/home/context/page.tsx
+++ app/home/context/page.tsx
@@ -6,9 +6,12 @@ import useContextStore from '@/zustand/edit-pages/context-store';
 import { AttachToAction } from './attachAnAction';
 import { Separator } from '@/components/ui/separator';
 import { EditButton } from '@/components/edit-page-components/edit-button';
+import { DeleteButton } from '@/components/edit-page-components/delete-button';
 
 export default function ContextPage() {
-    const { createdContext, editClickedIndicator, setEditClickedIndicator } = useContextStore()
+    const { createdContext, editClickedIndicator, setEditClickedIndicator, deleteClickedIndicator, setDeleteClickedIndicator } = useContextStore()
+    const getDataEndpoint = '/api/context/getContextViaSearch'
+    const entity = 'Context'
 
     return (
         <div className='p-8'>
@@ -17,11 +20,31 @@ export default function ContextPage() {
             </div>
             <Separator className='my-4' />
             <div className='flex flex-col gap-y-4'>
-                {createdContext ? null :
-                    !editClickedIndicator ? <CreateButton objName="Context" form={<ContextForm />} /> : null}
-                {createdContext ? null :
-                    <EditButton getDataByIdEndpoint='/api/context/getById' renderEditForm={(data) => <ContextForm initialData={data} />} editClickedIndicator={editClickedIndicator}
-                        setEditClickedIndicator={setEditClickedIndicator} getDataEndpoint='/api/context/getContextViaSearch' entity='Context' />}
+                {!createdContext && !deleteClickedIndicator && (
+                    !editClickedIndicator ? <CreateButton objName="Context" form={<ContextForm />} /> : null
+                )}
+
+                {!createdContext && !deleteClickedIndicator && (
+                    <EditButton
+                        getDataByIdEndpoint='/api/context/getById'
+                        renderEditForm={(data) => <ContextForm initialData={data} />}
+                        editClickedIndicator={editClickedIndicator}
+                        setEditClickedIndicator={setEditClickedIndicator}
+                        getDataEndpoint={getDataEndpoint}
+                        entity={entity}
+                    />
+                )}
+
+                {!createdContext && !editClickedIndicator && (
+                    <DeleteButton
+                        getDataEndpoint={getDataEndpoint}
+                        entity={entity}
+                        deleteClickedIndicator={deleteClickedIndicator}
+                        setDeleteClickedIndicator={setDeleteClickedIndicator}
+                        deleteEndpoint='/api/context/delete'
+                    />
+                )}
+
             </div>
             <div className='flex justify-center '>
                 {createdContext ? <AttachToAction /> : null}
