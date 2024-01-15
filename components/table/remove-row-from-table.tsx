3a6026cbd57bf8cfb209e95f7de66830dcdb0042
Zachary Lee <leeyihong03@gmail.com>
2024-01-15 12:29:10 +0800

    implement remove row from table

---
+++ components/table/remove-row-from-table.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { X } from 'lucide-react';
+import { SearchAndSelectInterface } from '../edit-page-components/search';
+
+interface RemoveRowFromTableButtonProps {
+    removeFunction: (item: SearchAndSelectInterface) => void
+    item: SearchAndSelectInterface
+}
+
+export const RemoveRowFromTableButton: React.FC<RemoveRowFromTableButtonProps> = ({
+    removeFunction,
+    item
+}) => {
+    return (
+        <Button
+            size="default"
+            className='bg-red-600'
+            onClick={() => removeFunction(item)}
+        >
+            <X className='w-4 h-4' />
+        </Button>
+    )
+};
