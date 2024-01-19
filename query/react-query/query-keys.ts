f62425f67df92b942ebde32f44708f83041255e0
Zachary Lee <leeyihong03@gmail.com>
2024-01-19 15:22:41 +0800

    implement user functions

--- query/react-query/query-keys.ts
+++ query/react-query/query-keys.ts
@@ -12,3 +12,8 @@ export const RQ_GET_ACTION_BY_ID = '/api/action/getById';
 export const RQ_GET_POLICY_VIA_SEARCH = '/api/policy/getContextViaSearch'
 export const RQ_GET_ALL_POLICY = '/api/policy/getAll';
 export const RQ_GET_POLICY_BY_ID = '/api/policy/getById';
+
+//USER
+export const RQ_GET_USER_VIA_SEARCH = '/api/user/getContextViaSearch'
+export const RQ_GET_ALL_USER = '/api/user/getAll';
+export const RQ_GET_USER_BY_ID = '/api/user/getById';
---
+++ zustand/edit-pages/user-store.ts
@@ -0,0 +1,36 @@
+import { User } from '@/lib/interface';
+import { create } from 'zustand';
+
+interface UserStore {
+    createdUser: User | null;
+    setCreatedUser: (User: User | null) => void;
+    editClickedIndicator: boolean;
+    setEditClickedIndicator: (clicked: boolean) => void;
+    deleteClickedIndicator: boolean;
+    setDeleteClickedIndicator: (clicked: boolean) => void;
+}
+
+const useUserStore = create<UserStore>(set => ({
+    // createdUser: null,
+    createdUser: {
+        jsonCol: {
+            name: 'John Doe',
+            age: 30,
+            occupation: 'Engineer',
+        },
+        applicationUserId: '012345',
+        id: '001',
+        modifiedDate: new Date(), // Current date and time
+        createdDate: new Date('2023-01-01'), // Specific date
+    },
+    setCreatedUser: (User: User | null) =>
+        set({ createdUser: User }),
+    editClickedIndicator: false,
+    setEditClickedIndicator: (clicked: boolean) =>
+        set({ editClickedIndicator: clicked }),
+    deleteClickedIndicator: false,
+    setDeleteClickedIndicator: (clicked: boolean) =>
+        set({ deleteClickedIndicator: clicked }),
+}));
+
+export default useUserStore;
