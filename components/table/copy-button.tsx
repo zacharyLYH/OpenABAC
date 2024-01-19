ec6c86e76fcf3cf29b50e5d8c433a73cb6390eff
Zachary Lee <leeyihong03@gmail.com>
2024-01-19 11:50:33 +0800

    style copy button with transition effect

--- components/table/copy-button.tsx
+++ components/table/copy-button.tsx
@@ -1,18 +1,25 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Copy } from 'lucide-react';
+import { CheckCircle, Copy } from 'lucide-react';
 import { objectToString } from './helper';
 import { toast } from 'sonner';
+import { useState } from 'react';
 
 interface CellActionInterface {
     data: any;
 }
 
 export const CopyButton: React.FC<CellActionInterface> = ({ data }) => {
+    const [copyClicked, setCopyClicked] = useState(false);
+
     const onCopy = (copyable: string) => {
         navigator.clipboard.writeText(copyable);
+        setCopyClicked(true);
         toast('Copied successfully!');
+        setTimeout(() => {
+            setCopyClicked(false);
+        }, 1000);
     };
 
     return (
@@ -20,9 +27,19 @@ export const CopyButton: React.FC<CellActionInterface> = ({ data }) => {
             size="sm"
             variant="secondary"
             onClick={() => onCopy(objectToString(data))}
+            className={
+                copyClicked
+                    ? "min-w-[80px] transition-background bg-green-500 text-white duration-1000"
+                    : "min-w-[80px]"
+            }
         >
-            <Copy className="mr-2 h-4 w-4" />
-            Copy
+            {copyClicked ?
+                <CheckCircle className='h-4 w-4' /> :
+                <>
+                    <Copy className="mr-2 h-4 w-4" />
+                    Copy
+                </>
+            }
         </Button>
     );
 };
