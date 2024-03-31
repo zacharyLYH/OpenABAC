c6e6636606e8300b82977c3eea79fa10231f1aed
Zac <yihongle@buffalo.edu>
2024-03-31 22:08:05 +0800

    implement COUNT_NUMBER_OF_POLICY_USING_ABACID

--- abac/core-queries/user-policies/user-policy.ts
+++ abac/core-queries/user-policies/user-policy.ts
@@ -15,3 +15,7 @@ DELETE FROM UserPolicy WHERE abacId = ?;
 export const CREATE_USER_POLICY_GIVEN_ABACID = `
 INSERT INTO UserPolicy (id, abacId, policyId) VALUES (UUID(), ?, ?);
 `;
+
+export const COUNT_NUMBER_OF_POLICY_USING_ABACID = `
+SELECT count(*) AS count from UserPolicy WHERE abacId = ?;
+`;
